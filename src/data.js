6e06662cb29089061a95f8d42a237b83d7860796
María Rodríguez <lu.rzapien.92@gmail.com>
2022-12-12 09:08:43 -0600

    Cambié estilo

--- src/data.js
+++ src/data.js
@@ -1,9 +1,15 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+// export const example = () => {
+//   return 'example';
+// };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+export const getBooks = (data) => {
+  return data.books;
+  
+ 
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { example } from './data.js';
+import { getBooks } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+import data from "./data/harrypotter/data.js";
 
-console.log(example, data);
+//console.log(example, data);
+const books = getBooks(data)
+console.log(data);
+console.log(books);
