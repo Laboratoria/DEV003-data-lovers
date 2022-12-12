17c387901059dc2b90c00c1d1dfb6b4bd91e8843
María Rodríguez <lu.rzapien.92@gmail.com>
2022-12-12 12:48:47 -0600

    Primera implementación de Data

--- src/main.js
+++ src/main.js
@@ -5,6 +5,25 @@ import { getBooks } from './data.js';
 import data from "./data/harrypotter/data.js";
 
 //console.log(example, data);
-const books = getBooks(data)
-console.log(data);
-console.log(books);
+// const books = getBooks(data)
+// console.log(data);
+// console.log(books);
+const showBooks = () => {
+  const books = getBooks(data);
+  const booksContainer = document.getElementById("librosContenedor");
+
+  for (let i = 0; i < books.length; i++) {
+    const book = books[i];
+    booksContainer.innerHTML +=
+      "<section class='libros'>" +
+        "<div class='facefront'>" +
+          "<img src='../imagenes/Harry_Potter_y_el_Misterio_del_Pr3Fol.PNG.webp' alt=''>" +
+          "<h3>" + book.title + "</h3>" +
+        "</div>" +
+      "</section>";
+  }
+}
+
+if (location.pathname === '/pages/libros') {
+  showBooks()
+}
