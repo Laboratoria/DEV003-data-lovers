69574891263abdf22779fb75b19bfc16ea70d5d0
María Rodríguez <lu.rzapien.92@gmail.com>
2022-12-12 20:04:13 -0600

    Implementación de la página -libros- usando data

--- src/data/harrypotter/data.js
+++ src/data/harrypotter/data.js
@@ -16185,7 +16185,7 @@ export default {
     {
       "id": 3,
       "title": "Harry Potter and the Prisoner of Azkaban",
-      "releseDay": "July 8, 1999",
+      "releaseDay": "July 8, 1999",
       "author": "J. K. Rowling",
       "description": "Harry's third year of studies at Hogwarts is threatened by Sirius Black's escape from Azkaban prison. Apparently, it is a dangerous wizard who was an accomplice of Lord Voldemort and who will try to take revenge on Harry Potter."
     },
--- src/main.js
+++ src/main.js
@@ -5,22 +5,54 @@ import { getBooks } from './data.js';
 import data from "./data/harrypotter/data.js";
 
 //console.log(example, data);
-// const books = getBooks(data)
-// console.log(data);
-// console.log(books);
+//const books = getBooks(data)
+//console.log(data);
+//console.log(books);
 const showBooks = () => {
   const books = getBooks(data);
-  const booksContainer = document.getElementById("librosContenedor");
+  const booksContainer = document.getElementById("booksContainer");
+
 
   for (let i = 0; i < books.length; i++) {
     const book = books[i];
-    booksContainer.innerHTML +=
-      "<section class='libros'>" +
-        "<div class='facefront'>" +
-          "<img src='../imagenes/Harry_Potter_y_el_Misterio_del_Pr3Fol.PNG.webp' alt=''>" +
-          "<h3>" + book.title + "</h3>" +
-        "</div>" +
-      "</section>";
+    const modalContainer = document.createElement("div");
+    const modalCloseBtn = document.createElement("button");
+    const modalTitle = document.createElement("h3");
+    const modalDescription = document.createElement("p");
+    const modalAuthor = document.createElement("span");
+    const modalReleaseDay = document.createElement("span");
+    const section = document.createElement("section");
+    const newFaceFront = document.createElement("div");
+    const bookTitle = document.createElement("h3");
+
+    modalContainer.className = "modal";
+    modalCloseBtn.className = "modal-btn";
+    modalCloseBtn.innerText = "Close";
+    modalContainer.style.display = "none";
+    modalTitle.innerText = book.title;
+    modalDescription.innerText = book.description;
+    modalAuthor.innerText = book.author;
+    modalReleaseDay.innerText = book.releaseDay;
+    section.className = "books";
+    newFaceFront.className = "facefront";
+    bookTitle.innerText = book.title;
+
+    booksContainer.appendChild(section);
+    section.appendChild(newFaceFront);
+    newFaceFront.appendChild(bookTitle);
+    modalContainer.appendChild(modalTitle);
+    modalContainer.appendChild(modalDescription);
+    modalContainer.appendChild(modalAuthor);
+    modalContainer.appendChild(modalReleaseDay);
+    booksContainer.appendChild(modalContainer);
+    modalContainer.appendChild(modalCloseBtn);
+
+    section.addEventListener("click", function () {
+      modalContainer.style.display = "block";
+    })
+    modalCloseBtn.addEventListener("click", function () {
+      modalContainer.style.display = "none";
+    })
   }
 }
 
