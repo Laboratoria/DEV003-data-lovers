3ef9825ba599bcb0b953d297b2a37ab26a122684
Maria Feranda Choreno Teran <mafcht@MacBook-Air-de-Maria.local>
2022-12-15 16:50:47 -0600

    Ver y ocultar modales parte 1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { example } from './data.js';
+import { pokemon } from './data.js';
 // import data from './data/lol/lol.js'; No estamos usando esta data
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js'; No estamos usando esta data
-
 console.log(example, data);
+const paginaPrincipal = document.getElementById("introduction");
+const buttonHome = document.getElementById("btnHome")
+
+buttonHome.addEventListener("click", function(){
+    document.getElementById("introduction").style.display = "block";
+  });
