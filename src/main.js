b7e208cda79ebad2aedabc7294449593883cdcac
Maria Feranda Choreno Teran <mafcht@MacBook-Air-de-Maria.local>
2022-12-15 13:02:43 -0600

    Primeros cambios en header

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
+// import data from './data/lol/lol.js'; No estamos usando esta data
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// import data from './data/rickandmorty/rickandmorty.js'; No estamos usando esta data
 
 console.log(example, data);
