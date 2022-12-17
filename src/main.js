474c2e948ebf71daaa3a0a8ddd06eb77c5057adf
RossannaAriza <rossanna.ariza@gmail.com>
2022-12-17 12:29:09 -0500

    se agregaprimera funcion a boton

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
+
+const pasarPokemon = document.querySelector("#btnPokemones")
+const sectionPage1 = document.querySelector("#page1");
+const sectionPage2 = document.querySelector("#page2");
+
+pasarPokemon.addEventListener("click", function(){
+  sectionPage1.style.display= "none";
+  sectionPage2.style.display= "block";
+})
