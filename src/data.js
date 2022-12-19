d1aff35335dd32150729a0d6271c8795f0b78d24
María Rodríguez <lu.rzapien.92@gmail.com>
2022-12-19 11:54:30 -0600

    Modificaciones en el estilo

--- src/data.js
+++ src/data.js
@@ -1,5 +1,4 @@
 // estas funciones son de ejemplo
-
 // export const example = () => {
 //   return 'example';
 // };
@@ -12,6 +11,17 @@ export const getBooks = (data) => {
   return data.books;
 }
 
-export const getCharacters= (data) => {
+export const getCharacters = (data) => {
   return data.characters;
+
+}
+
+const searchCharacterByName = (name, characters) => {
+  if(name === "") return []
+
+  const charactersFound = characters.filter(function(character){
+    const characterName = character.name.toLowerCase();
+    return characterName.includes(name.toLowerCase());
+  })
+  return charactersFound
 }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import { getBooks } from './data.js';
+import { getBooks, getCharacters, searchCharacterByName  } from './data.js';
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 import data from "./data/harrypotter/data.js";
 
+
+
 //console.log(example, data);
 //const books = getBooks(data)
 //console.log(data);//
-//console.log(books);
+console.log(data.books);
+console.log(data.characters);
 const showModalBooks = (title, description, author, releaseDay) => {
   const modalContainer = document.getElementById("modalContainer");
   const modalTitle = document.getElementById("modalTitle");
@@ -44,7 +47,7 @@ const showBooks = () => {
     bookIcon.height = 64;
     section.className = "card-book";
     bookTitle.innerText = book.title;
-    bookTitle.className = "titulo"
+    bookTitle.className = "titulo";
 
     booksContainer.appendChild(section);
     section.appendChild(bookIcon)
@@ -56,6 +59,40 @@ const showBooks = () => {
   }
 }
 
+
 if (location.pathname === '/pages/libros' || location.pathname === '/DEV003-data-lovers/pages/libros.html') {
   showBooks()
+}
+const showCharacters = () => {
+  const characters = getCharacters(data);
+  //characters.forEach((character) => console.log(character.name));
+  const charactersContainer = document.getElementById("charactersContainer");
+  const form = document.getElementById("charactersSearchForm");
+  form.addEventListener("submit",  function (e) {
+    e.preventDefault();
+    charactersContainer.innerHTML = "";
+    const searchVal = form.querySelector(".search").value;
+    const charactersFound = searchCharacterByName(searchVal,characters);
+
+    if(charactersFound.length > 0){
+      charactersFound.forEach(function(character){
+        const nameEl = document.createElement("h3");
+        nameEl.className = "character-name";
+        nameEl.innerText = character.name;
+        charactersContainer.appendChild(nameEl);
+      });
+    }else{
+      const notFoundEl = document.createElement("h3");
+      notFoundEl.className = "character-name";
+      notFoundEl.innerText = "we have not found any character with: " + '"' + searchVal + '"';
+      charactersContainer.appendChild(notFoundEl);
+    }
+    console.log(charactersFound)
+  })
+  
+}
+
+
+if (location.pathname === '/pages/personajes') {
+  showCharacters()
 }
