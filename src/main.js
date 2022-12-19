be0c562557cf09c441fe836c7e09430ead88f868
Fabiana Hasu <fabi.hasu@gmail.com>
2022-12-19 14:46:36 -0300

    versión 1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/harrypotter/data.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+const personaje = data.characters
+console.log(personaje, data)
+
+
+//function getName() {
+    const name= document.getElementById("informationbox").value
+
+
+//console.log(getName)
