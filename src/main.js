0944ca7747121b6a06011be101f28992d0f9df3b
RossannaAriza <rossanna.ariza@gmail.com>
2022-12-20 15:45:14 -0500

    agrega estructura en cambio HTML, funcion a un boton y estilo en CSS primer prototipo

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import data from './data/pokemon/pokemon.js';
 
 //console.log(example, data);
 
-const pasarPokemon = document.querySelector("#btnPokemones")
+const btnPokemon = document.querySelector("#btnPokemones")
 const sectionPage1 = document.querySelector("#page1");
 const sectionPage2 = document.querySelector("#page2");
+const sectionPage3 = document.querySelector("#page3");
+const sectionPage4 = document.querySelector("#page4");
 
-pasarPokemon.addEventListener("click", function(){
+btnPokemon.addEventListener("click", function(){
   sectionPage1.style.display= "none";
   sectionPage2.style.display= "block";
+  sectionPage3.style.display= "none";
+  sectionPage4.style.display= "none";
 })
