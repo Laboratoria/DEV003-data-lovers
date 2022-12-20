e34257d935ba2b899a00f5c9f5df17ac7e0c919c
Maria Feranda Choreno Teran <mafcht@MacBook-Air-de-Maria.local>
2022-12-20 09:55:13 -0600

    cambios fernanda

--- src/data.js
+++ src/data.js
@@ -8,9 +8,15 @@
 //  return 'OMG';
 //};
 
-export const name = (data) => {
-return 'name';
+export const getPokemon = (data) => {
+return data.pokemon;
 };
+
+export const getName = (data) => {
+    return data.name;
+    };
+    
+//const ascendente = data.pokemon.sort(name);
 
 
 //export const atributo = element.getAttribute(pokemon.num);
@@ -18,5 +24,8 @@ return 'name';
 
 //comentario fer usar filterData(data, condition):   ,   sortData(data, sortBy, sortOrder):    ,   computeStats(data):
 //arr.sort((a, b) => a - b);  // WILL SORT ASC 
+
+//data.pokemon.sort((a, b) => a - .name);  // WILL SORT ASC 
+
 //arr.sort((a, b) => b - a);  // WILL SORT DESC
 //arr.sort()                  // WILL SORT STRINGS ASC 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 //import { example } from './data.js';
+import { getName } from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 //console.log(example, data);
+//console.log(data);
+//console.log(data.pokemon);
+//console.log(data.pokemon[0].generation.name);
+console.log(data.name);
 
 const buttonHome = document.getElementById("btnHome")
 const buttonPokedex = document.getElementById("btnPokedex")
@@ -16,4 +21,3 @@ buttonHome.addEventListener("click", function(){
   document.getElementById("scIntroduction").style.display = "none";
 });
 
-console.log(name);
