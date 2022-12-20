e475767f3608832b6e3fd440f14cb9614954c81f
Monica Romero <monicaromerogay@gmail.com>
2022-12-20 12:48:57 -0600

    cambios finales para sprit 1

--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
 import { example } from './data.js';
+// import data from './data/lol/lol.js';
 import data from './data/harrypotter/data.js';
+// import data from './data/rickandmorty/rickandmorty.js';
 
-function getTheName() {
-const name = document.getElementById("informationbox")
+console.log(example, data);
+
+const personajes = data.characters
+
+document.getElementById("search").addEventListener("click", buscar)
+
+function buscar() {
+  const name = getName()
+  console.log(name)
+  for (let i = 0; i < personajes.length; i++) {
+    if (personajes[i].name === name) {
+      alert("Este personaje es:" + personajes[i].house)
+
+    }
+  }
+
+  function getName() {
+    return document.getElementById("informationbox").value
+  }
+
 }
-console.log(getTheName);
 
-
-
-//console.log(example, data);
-
-//const personaje = data.characters
-//console.log(personaje,data)
 
 
