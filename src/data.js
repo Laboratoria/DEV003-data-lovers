caefd07671d33ea5e629dfd8f7fe5bddc25945c7
Chris <chris.olivosf@gmail.com>
2022-12-20 09:31:27 -0500

    funcion para mostrar todos los pokemones parte 1

--- src/data.js
+++ src/data.js
@@ -8,15 +8,38 @@
 //  return 'OMG';
 //};
 
-export const name = (data) => {
-return 'name';
+export const getDataPokemon=(data)=>{
+
+if (data === undefined || typeof data !== 'object' || data === 0 || data === null || data.length === 0 || data ===''){ 
+  throw new TypeError('data is not an object');
+}
+
+//Variable que guarda la data Pokémon
+const pokemonList = data.pokemon;
+const allDataPokemon = (pokemonList) => {
+const pokeCard = document.getElementById("myModal");
+//ForEach recorre cada elemento del array.
+// Función para mostrar todos los pokémon, se utiliza innerHTML para mostrar la información en pantalla.
+pokemonList.forEach((elem) => {
+ 
+     //  console.log(elem); <img class="picture" src="${elem.img}"></img>
+     pokeCard.innerHTML+= `
+    
+     <div>
+     <div id="card">
+       <div id="front" > 
+          <div > ${elem.name.toUpperCase()}</div>
+          <div >#${elem.num}</div>
+          
+       </div>
+ `;
+
+  
+});
 };
+  
+return allDataPokemon;
 
+    
+}
 
-//export const atributo = element.getAttribute(pokemon.num);
-
-
-//comentario fer usar filterData(data, condition):   ,   sortData(data, sortBy, sortOrder):    ,   computeStats(data):
-//arr.sort((a, b) => a - b);  // WILL SORT ASC 
-//arr.sort((a, b) => b - a);  // WILL SORT DESC
-//arr.sort()                  // WILL SORT STRINGS ASC 
