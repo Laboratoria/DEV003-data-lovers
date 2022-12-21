5327ba069937add470a57bfeaa307f507d8e7aa0
NataliaAspee <n.aspeedelgado@gmail.com>
2022-12-21 19:17:08 -0300

    Html-update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,2 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-console.log(example, data);
+import data from './data/ghibli/ghibli.js'
+//console.log(ghibli, data);
