24cbe88cd84741c05be1d3dd53764e6c6acf9b7c
Natalia Aspee <n.aspeedelgado@gmail.com>
2022-12-23 13:08:43 -0300

    prueba repositorios remoto

--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
+//import ghibli from './data/ghibli/ghibli.js'
 import data from './data/ghibli/ghibli.js'
-//console.log(ghibli, data);
+//dom
+
+//console.log(data.films) //funciona tomando las peliculas 
+const dataMovies = data.films
+dataMovies.forEach(function(film) 
+{
+  //parametro title, como llamar people
+  console.log(film.director) 
+})
+
+
+
+//specie funcion filter para HU penelope sub categoria
+//director funcion filter HU Omar
+//title para sort? HU camila
+//release_date sort? HU camila
+
