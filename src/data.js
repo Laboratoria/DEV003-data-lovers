1f1687319007f7bc9b84e5309e203c651c2ec019
sara040616 <sara.sierraramos@gmail.com>
2022-12-22 22:01:59 -0500

    mostrar la data en la interfaz

--- src/data.js
+++ src/data.js
@@ -1,8 +1,11 @@
 // estas funciones son de ejemplo
+import dataPokemons from "./main.js";
+const elemento = document.getElementById("filtroTipo");
+export const filtroData = dataPokemons.filter(dataPokemons =>data.type === elemento) {
+  return filtroData;
+  console.log(filtroData)
+} 
 
-export const example = () => {
-  return 'example';
-};
 
 export const anotherExample = () => {
   return 'OMG';
