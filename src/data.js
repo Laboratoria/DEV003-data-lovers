456c15f203d2974e6a8e4316aa8f214b31203d9a
Chris <chris.olivosf@gmail.com>
2022-12-23 20:31:03 -0500

    muestra data por type y region

--- src/data.js
+++ src/data.js
@@ -10,55 +10,35 @@ export const getDataPokemon = () => { // exportamos funcion array
 
 export const orderByOption = (valor) => { //exportamos funcion de ordenar por opcion
   const pokemonSortBy = data['pokemon'];
-  if (valor === "3") {
-    pokemonSortBy.sort((a, b) => a.name.localeCompare(b.name));
-  }
-  else if (valor === "4") {
-    pokemonSortBy.sort((a, b) => b.name.localeCompare(a.name));
-  }
-  else if (valor === "2") {
-    pokemonSortBy.sort((a, b) => b.num.localeCompare(a.num));
-  }
-  else if (valor === "1") {
+  if (valor === "1") {
     pokemonSortBy.sort((a, b) => a.num.localeCompare(b.num));
-  }
+  }else if (valor === "2") {
+    pokemonSortBy.sort((a, b) => b.num.localeCompare(a.num));
+  }else if (valor === "3") {
+    pokemonSortBy.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (valor === "4") {
+    pokemonSortBy.sort((a, b) => b.name.localeCompare(a.name));
+  } 
   return pokemonSortBy;
 };
 
 //funcion de filtrar data  filterData(data, condition)
-
+  //1 traer el valor de la lista de tipos
+    //2 recorrer la data completa
+    //3 comparar que items de la data cumplen con el select de la lista
+    //4 crear array para devolver las cards
 export const filterData = (filterBy, condition ) => {
   const pokemonArray = data['pokemon'];
   let result = [];
   if(filterBy === 'type'){
     result = pokemonArray.filter(pokemon => pokemon.type.includes(condition));
-  }else {
-    result = pokemonArray.filter(pokemon => pokemon.name.includes(condition));
+  }else if(filterBy === 'kanto') {
+    result = pokemonArray.filter(pokemon => pokemon.generation.name.includes(condition));
+  }else if(filterBy === 'johto') {
+    result = pokemonArray.filter(pokemon => pokemon.generation.name.includes(condition));
   }
-  console.log(result)
+ // console.log(result)
   return result;
 };
 
-  /*
-  export const filterData = (valor) => {
-  console.log(valor);
-const pokemonFilter = data['pokemon'];
-  let result = [];
-  for (let i=0 ; i< pokemonFilter.length ; i++){
-    //1 traer el valor de la lista de tipos
-    //2 recorrer la data completa
-    //3 comparar que items de la data cumplen con el select de la lista
-    //4 crear array para devolver las cards
-    let pokemonType = pokemonFilter.type
-    result.push(pokemonType[i])
-    
-   }
-  if (pokemonFilter === valor) {
-    result = pokemonArray.filter(pokemon => pokemon.type.includes(valor));
-  }/*else {
-    result = pokemonArray.filter(pokemon => pokemon.name.includes(condition));
-  }
-  console.log(result)
-  return result;
-};
-*/
+  
