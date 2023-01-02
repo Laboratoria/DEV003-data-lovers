e9fd9777ab6b8a9ee5155cfada475e4f434848c1
Johana Gonzales <j.gonzalesrav@gmail.com>
2023-01-02 09:58:04 -0500

    subiendo ultimos cambios de diciembre

--- src/data.js
+++ src/data.js
@@ -9,28 +9,43 @@ import DataGlibli  from './data/ghibli/ghibli.js';
 export const anotherExample = () => {
   return 'OMG';
 };*/
+
 function ChargeGhibli(){
   //console.log(DataGlibli.films[1]);
   const container = document.querySelector(".container");
   for(let i = 0; i <= DataGlibli.films.length; i++){
-    const div = document.createElement("div");
-    div.className = "card"; 
+    const divCard = document.createElement('div');
+    divCard.className = "card"; 
+    
+    const divCardFront = document.createElement('div')
+    divCardFront.className = "face front"
+    
     const img = document.createElement('img');
     img.src = DataGlibli.films[i].poster;
-    const title = document.createElement("span");
+    
+    const title = document.createElement('h3');
     title.className = "title";
     title.textContent =  DataGlibli.films[i].title;
-    //const description = document.createElement("span");
-    //description.textContent = DataGlibli.films[i].description;
+    
+    const divCardBack = document.createElement('div');
+    divCardBack.className = "face back"
+    
+    const description = document.createElement('p');
+    description.className = "description"
+    description.textContent = DataGlibli.films[i].description;
     //console.log("container", container);
   
-    div.append(img);
-    div.append(title);
-    //div.append(description);
-    container.append(div);
+    container.append(divCard);
+    divCard.append(divCardFront, divCardBack);
+    divCardFront.append(img, title);
+    divCardBack.append(description);
+    
+    divCard.addEventListener("click", function() {
+      this.classList.toggle("card");
+    });
   }
-
+  // Handle click on touchscreens
+  //const flip_card = document.querySelector(".");
 }
 
 ChargeGhibli();
-
