a2ad9752f3906ce68ec29ab2985262c7f3cbee8c
MenaVillalobos <jimee69styles@gmail.com>
2023-01-02 17:59:52 -0600

    starting with characters cards

--- src/main.js
+++ src/main.js
@@ -5,10 +5,42 @@ import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
 
-const characters=data.results;
-const filter=characters.filter((character) => {
-    
-    return character.id===200
-})
+//Implementar método que recorra toda mi data de R&M
+for (let character of data.results){
+    console.log(character.name);
+    //Crear card de molde para reutilizar
+    let cardMold=document.createElement("div");
+    //Agregando datos/categoría a mi card
+    cardMold.classList.add("cardMold");
+    //Agregar img a la card
+    let cardImg=document.createElement("div");
+    cardImg.classList.add("card-img");
+    //Mandar a traer la img
+    let img=document.createElement("img"); //Crea elemento img
+    img.setAttribute("src", character.image);
+    cardImg.appendChild(img); //Anida img en cardImg
+    cardMold.appendChild(cardImg); //Anida en cardMold
 
-console.log(filter);
+    document.getElementById("cards").appendChild(cardMold); //Anida en cards
+
+    //Mostrando nombre
+    //Creando div para contenedor de nombre
+    let container=document.createElement("div");
+    container.classList.add("container");
+    //Characters name
+    let charName=document.createElement("h1");
+    charName.classList.add("name");
+    charName.innerText=character.name;
+
+    container.appendChild(charName);
+
+    cardMold.appendChild(container);
+}
+
+//const characters=data.results;
+// const filter=characters.filter((character) => {
+    
+//     return character.id===200
+// })
+
+// console.log(filter);
