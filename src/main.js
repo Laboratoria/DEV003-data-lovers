651127e971a88e85496dd0695e9f27d5a57ea3b8
mariahiguera2210 <angelica.higueram92@gmail.com>
2023-01-03 22:25:56 -0500

    filtro map en main.js

--- src/main.js
+++ src/main.js
@@ -1,32 +1,32 @@
-// import { characters } from './data.js';
-//import harrypotter from './data/pokemon/pokemon.js';
-// import data from './data/lol/lol.js';
 import data from './data/harrypotter/data.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-//import data from ';
-//console.log(example, pokemon);
 
-// console.log(data)
 
-//const filtros=data.funFacts
-//console.log(filtros)
 
-const personajes=data.characters.filter(function(personaje){
-    return personaje.species 
+var personajes= data.characters.map(function(personaje){
+    return {
+
+        hola: personaje.name,
+        especial: personaje.species,
+        genero: personaje.gender,
+        casa: personaje.house
+
+  
+    }
 })
 
 console.log(personajes)
 
 
-var especies=personajes.map(function(especie){
-    return especie.species
+
+var nombres= data.characters.find(function(nombre){
+    return nombre.name === "Hermione Granger"
 })
 
-console.log(especies)
+console.log(nombres)
 
-let especiesU = especies.filter((item,index)=>{
-    return especies.indexOf(item) === index;
-  })
-  console.log(especiesU);
+// let especiesU = especies.filter((item,index)=>{
+//     return especies.indexOf(item) === index;
+//   })
+//   console.log(especiesU);
 
-  console.log(especiesU.length)
+//   console.log(especiesU.length)
