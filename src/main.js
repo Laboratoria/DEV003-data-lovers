7d38f9e8bbceb09f9af3529dd0a5817b9f5b195b
Monica Romero <monicaromerogay@gmail.com>
2023-01-05 10:31:37 -0600

    cambios a ingles

--- src/main.js
+++ src/main.js
@@ -13,17 +13,23 @@ function buscar() {
   const name = getName()
   console.log(name)
   for (let i = 0; i < personajes.length; i++) {
-    if (personajes[i].name === name) {
-      alert("Este personaje es:" + personajes[i].house)
+    if (personajes[i].name.toUpperCase () === name.toUpperCase ()) {
+
+      const mensaje = "This character is: " + personajes[i].name + ". It belongs to House " + personajes[i].house
+      // alert(mensaje)
+
+      getDivMensajeUsuario().innerHTML = `<div id='divmensaje'>${mensaje}</div>`
 
     }
   }
-
-  function getName() {
-    return document.getElementById("informationbox").value
-  }
-
 }
 
+function getDivMensajeUsuario() {
+  return document.getElementById("mensajeUsuario")
+}
 
+function getName() {
+  return document.getElementById("informationbox").value
+}
 
+alert("Este personaje es:" + personajes[i].house)
