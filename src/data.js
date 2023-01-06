1e59f31802a47b923e39d99d085b9afbd86e96ce
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-01-05 21:04:52 -0300

    prueba funcion filter directores

--- src/data.js
+++ src/data.js
@@ -1,16 +1,18 @@
 import data from './data/ghibli/ghibli.js'
-//const dataMovies = data.films;
+const dataMovies = data.films;
 //funciona pero toma todos los elementos del array, lo muestra como un objeto 
-/*
-export function randomMovie(dataMovies){
-  return dataMovies[Math.floor(Math.random()* dataMovies.length)]
+
+export const Miyazaki = dataMovies.filter(function(dataMovies){
+  return dataMovies.director === 'Hayao Miyazaki'
 }
-*/
+else if ()
 
-/* como mejorar la funcion para cada uno de los directores
+);
 
-const Miyazaki = dataMovies.filter((dataMovies) => dataMovies.director === 'Hayao Miyazaki'
-); 
-console.log(Miyazaki)
+// como mejorar la funcion para cada uno de los directores
+/* export const Miyazaki = dataMovies.filter((dataMovies) => {
+  return dataMovies.director === 'Hayao Miyazaki'
+});
+
 //funcionando, retorna solo peliculas de miyazaki pero el array  */
 
--- src/main.js
+++ src/main.js
@@ -1,23 +1,25 @@
 //import ghibli from './data/ghibli/ghibli.js'
+import { Miyazaki } from './data.js'
 import data from './data/ghibli/ghibli.js'
-//import { randomMovie } from './data.js'
+
 const dataMovies = data.films;
-//declaramos donde se mostraran las peliculas
 const moviesList = document.getElementById('showMovies');
-//declaramos el html vacio para luego insertar con innerHtml
 let htmlCode = ``;
-//tomamos cada dato del array con un forEach, recorre todo el array y retorna los datos que elegimos más abajo
-dataMovies.forEach(function(singleMovieObjects)
-{
-  //imagen de arriety link roto
-  //elementos que se insertaran desde la data al html 
-  htmlCode = htmlCode +
+
+//declaramos funcion para mostra la data de las peliculas mediante un innerHTML
+function printMovies(){
+  //declaramos el html vacio para luego insertar con innerHtml
+  //tomamos cada dato del array con un forEach, recorre todo el array y retorna los datos que elegimos más abajo
+  dataMovies.forEach(function(singleMovieObjects)
+  {
+  //elementos que se insertaran desde la data al html
+  //html += not an addition, but a full overwrite. 
+    htmlCode = htmlCode +
     `
       <div>
       
       <img src="${singleMovieObjects.poster}">
       <p>Title: ${singleMovieObjects.title}</p>
-      <p>Description: ${singleMovieObjects.description}</p>
       <p>Director: ${singleMovieObjects.director}</p>
       <p>Producer: ${singleMovieObjects.producer}</p>
       <p>Release date: ${singleMovieObjects.release_date}</p>
@@ -25,14 +27,26 @@ dataMovies.forEach(function(singleMovieObjects)
       </div>
 
   `;
-  /*console.log(htmlCode); funciona, luego tomamos movieList definido más arriba para insertarlo
-//we now say let the inside of that div take in our htmlCode variable that holds our html codes.*/
-});
-moviesList.innerHTML= htmlCode
+    //we now say let the inside of that div take in our htmlCode variable that holds our html codes.*/
+  });
+  moviesList.innerHTML= htmlCode
+}
+printMovies(dataMovies);
+
+console.log(Miyazaki)
+
+//filtra solo Miyazaki
+//Miyazaki.forEach(function(film){
+//console.log(film.title +' '+ film.director + ' ' + film.release_date)});
 
 /*
 //specie funcion filter para HU penelope sub categoria
 //director funcion filter HU Omar
 //title para sort? HU camila
 release_date sort? HU camila
+*/
+
+/* retorna lo que indica el console log desde
+dataMovies.forEach(function(film){
+  console.log(film.title +' '+ film.director + ' ' + film.release_date)});
 */
