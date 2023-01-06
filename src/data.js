4b59a5aa36f5653976c2636b54fce095d9efe30b
LittlePuggers <anaim.ecologia@gmail.com>
2023-01-06 13:55:02 -0700

    Pasamos todo a un solo html, css y se elimino characters.js

--- src/data.js
+++ src/data.js
@@ -2,81 +2,22 @@ import data from "./data/harrypotter/data.js";
 
 const { characters } = data;
 
-// estas funciones son de ejemplo
+let filtered = characters;
+sortMe(filtered); //llama la funcion para ordenar, es la primera que corre
 
-export const personajes = () => {
-  return characters;
-};
+//para ordenar alfabeticamente
+function sortMe(items) {
+  items.sort((a, b) => {
+    const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
 
-//como meter un elemento a un array [].push
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
-
-console.log(characters.sort());
-//const { characters } = data;
-const { spells } = data;
-
-export const example = (id) => {
-  return characters[id].name;
-};
-
-//como meter un elemento a un array ([].push)
-//como condicionar para que no se repitan los elementos en el array (if..)
-//como accesar a la información que necesito
-//aplicar la condición que evita que se repitan los elementos que van a ser empujados
-
-let species = [];
-
-for (let i = 0; i < characters.length; i++) {
-  const currentSpecies = characters[i].species?.toLowerCase();
-  if (species.includes(currentSpecies) === false) {
-    species.push(currentSpecies);
-  }
+    // names must be equal
+    return 0;
+  });
 }
-
-console.log(species.sort());
-
-let gender = [];
-
-for (let i = 0; i < characters.length; i++) {
-  const currentGender = characters[i].gender?.toLowerCase();
-  if (gender.includes(currentGender) === false) {
-    gender.push(currentGender);
-  }
-}
-
-console.log(gender.sort());
-
-let house = [];
-
-for (let i = 0; i < characters.length; i++) {
-  const currentHouse = characters[i].house?.toLowerCase();
-  if (house.includes(currentHouse) === false) {
-    house.push(currentHouse);
-  }
-}
-
-console.log(house.sort());
-
-let ancestry = [];
-
-for (let i = 0; i < characters.length; i++) {
-  const currentAncestry = characters[i].ancestry?.toLowerCase();
-  if (ancestry.includes(currentAncestry) === false) {
-    ancestry.push(currentAncestry);
-  }
-}
-
-console.log(ancestry.sort());
-
-let spellType = [];
-
-for (let i = 0; i < spells.length; i++) {
-  const currentSpelltype = spells[i].spell_type?.toLowerCase();
-  if (spellType.includes(currentSpelltype) === false) {
-    spellType.push(currentSpelltype);
-  }
-}
-
-console.log(spellType.sort());
--- src/main.js
+++ src/main.js
@@ -1,35 +1,65 @@
-import data from './data/harrypotter/data.js';
+import data from "./data/harrypotter/data.js";
+const { characters } = data;
 
+const charactersPage = document.querySelector(".characters");
 
+const charactersDiv = document.querySelector(".showCharacters");
+const genderFilter = document.querySelector("#gender");
+const houseFilter = document.querySelector("#house");
+const clearFilter = document.querySelector("#limpiar");
 
-var personajes= data.characters.map(function(personaje){
-    return {
-
-        hola: personaje.name,
-        especial: personaje.species,
-        genero: personaje.gender,
-        casa: personaje.house
-
-  
-    }
-})
-
-console.log(personajes);
-
-var especies = personajes.map(function (especie) {
-  return especie.species;
+charactersPage.addEventListener("click", () => {
+  characters.map((character) => {
+    const personaje = document.createElement("div");
+    personaje.setAttribute("class", "character-card");
+    personaje.innerHTML = `<ul class="character-info">
+    <li>Name: ${character.name}</li>
+    <li>Birth: ${character.birth}</li>
+    <li>Species: ${character.species}</li>
+    <li>Gender: ${character.gender}</li>
+    <li>House: ${character.house}</li>
+    <li>Books: ${character.books_featured_in}</li>
+    </ul>`;
+    charactersDiv.append(personaje);
+  });
 });
 
+let filtered = characters;
 
-var nombres= data.characters.find(function(nombre){
-    return nombre.name === "Hermione Granger"
-})
+genderFilter.addEventListener("change", (e) => {
+  charactersDiv.replaceChildren(); //reemplaza todos los hijos por nada
+  filtered = characters.filter((character) => {
+    return character.gender === e.target.value;
+  });
+  filtered.map((character) => {
+    createCharEl(character);
+  });
+});
 
-console.log(nombres)
+houseFilter.addEventListener("change", (e) => {
+  charactersDiv.replaceChildren();
+  filtered = characters.filter((character) => {
+    return String(character.house) === e.target.value;
+  });
+  filtered.map((character) => {
+    createCharEl(character);
+  });
+});
 
-// let especiesU = especies.filter((item,index)=>{
-//     return especies.indexOf(item) === index;
-//   })
-//   console.log(especiesU);
+clearFilter.addEventListener("click", () => {
+  charactersDiv.replaceChildren();
+  characters.map((character) => {
+    createCharEl(character);
+  });
+});
 
-//   console.log(especiesU.length)
+document.addEventListener("keyup", (e) => {
+  if (e.target.matches("#buscador")) {
+    if (e.key === "Escape") e.target.value = " ";
+    document.querySelectorAll(".character-card").forEach((character) => {
+      character.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+        ? character.classList.remove("filtro")
+        : character.classList.add("filtro");
+    });
+  }
+});
