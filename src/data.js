6e2b1b0ae328b6cf4250428a95a98bc3f4609b83
alesantoscoz <116589040+alesantoscoz@users.noreply.github.com>
2023-01-09 14:11:14 -0500

    correciones

--- src/data.js
+++ src/data.js
@@ -3,52 +3,52 @@
 
 export function filterData(data, input, category){
   //Ejecuta la función si está dentro de estas categorías
-  if (category==="name"||category==="species"||category==="type"||category==="status") {
-    const filterD=data.filter((character) => {
+  if (category === "name" || category === "species" || category === "type" || category === "status") {
+    const filterD = data.filter((character) => {
       console.log(character[category]); //Imprime todos los personajes
-     
-      const selectOpt=character[category].toLowerCase(); //Opc seleccionada de select
-      const enteredTxt=input.toLowerCase(); //Txt ingresado en input
-  
+
+      const selectOpt = character[category].toLowerCase(); //Opc seleccionada de select
+      const enteredTxt = input.toLowerCase(); //Txt ingresado en input
+
       return selectOpt.includes(enteredTxt);
       //includes compara la data del select con el txt ingresado en el input
     })
     console.log(filterD);
-  return filterD;
-  } else if (category==="gender") {
+    return filterD;
+  } else if (category === "gender") {
     // otro filtro
-    const filtGender=data.filter((character) => {
-      const selectOpt=character[category].toLowerCase(); //Opc seleccionada de select
-      const enteredTxt=input.toLowerCase(); //Txt ingresado en input
-  
+    const filtGender = data.filter((character) => {
+      const selectOpt = character[category].toLowerCase(); //Opc seleccionada de select
+      const enteredTxt = input.toLowerCase(); //Txt ingresado en input
+
       return selectOpt.startsWith(enteredTxt);
       //comparar si la categoria de la data startsWith input del usuario
       //string.startsWith('Fe');
     })
     console.log(filtGender);
     return filtGender;
-  } else if (category==="origin"||category==="location"){
+  } else if (category === "origin" || category === "location") {
     //Otro filtro
-    const filtLoc=data.filter((character) => {
-      const selectOpt=character[category].name.toLowerCase();
-      const enteredTxt=input.toLowerCase();
+    const filtLoc = data.filter((character) => {
+      const selectOpt = character[category].name.toLowerCase();
+      const enteredTxt = input.toLowerCase();
       return selectOpt.includes(enteredTxt);
     })
     return filtLoc;
-  } else if (category==="episode"){
+  } else if (category === "episode") {
     //Otro filtro
-    const filtEpi=data.filter((character) => {
+    const filtEpi = data.filter((character) => {
       console.log(character[category]);//contiene mis elementos en un string
-      const enteredTxt=input.toLowerCase();//Contiene txt input
-      const epNumbs=character[category].map((episode)=>{//Tiene mi array de núms
-        const urlNum=episode.split("/");//Contiene array con elements separados
-        const lastNumb=urlNum[urlNum.length -1];
-        
+      const enteredTxt = input.toLowerCase();//Contiene txt input
+      const epNumbs = character[category].map((episode) => {//Tiene mi array de núms
+        const urlNum = episode.split("/");//Contiene array con elements separados
+        const lastNumb = urlNum[urlNum.length - 1];
+
         return lastNumb;
       })
       console.log(epNumbs);
-      
-        return epNumbs.includes(enteredTxt);
+
+      return epNumbs.includes(enteredTxt);
     })
     return filtEpi;
   }
@@ -56,19 +56,19 @@ export function filterData(data, input, category){
 
 export function orderAlf(orderType, inputArray) {
   if (orderType === "alphabetic") {
-     return inputArray.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+    return inputArray.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
   }
   if (orderType === "reverse") {
-     return inputArray.sort((a,b) => (a.name > b.name) ? -1 : ((b.name > a.name) ? 1 : 0))
-  
+    return inputArray.sort((a, b) => (a.name > b.name) ? -1 : ((b.name > a.name) ? 1 : 0))
+
   }
   return inputArray
 };
 
 export function getData(categoSelect, inpTxt, orderType, data) {
-      const filteredResult = filterData(data.results, inpTxt, categoSelect); //Lo que retorna filterData almacenado en filteredResult
-      const orderedResult = orderAlf(orderType, filteredResult)
-      return orderedResult
+  const filteredResult = filterData(data.results, inpTxt, categoSelect); //Lo que retorna filterData almacenado en filteredResult
+  const orderedResult = orderAlf(orderType, filteredResult)
+  return orderedResult
 };
 //Obtener valor de input txt en una variable---> Hecho
 
