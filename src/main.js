c5d69c9a33e41a604284b85a5a92a31b7c3dcbc3
RossannaAriza <rossanna.ariza@gmail.com>
2023-01-11 08:44:08 -0500

    agrega las funciones de mostrar data en interfaz ordenada o filtrada

--- src/main.js
+++ src/main.js
@@ -47,22 +47,20 @@ multiplicadorNav.addEventListener("click", function(){
 const dataPokemons = data.pokemon;
 
 //se declara una constante de la seccion de la pagina donde se visualizara el catalogo o biblioteca de pokemons
-const catalogo =document.getElementById("page2");
-
+const catalogo =document.getElementById("mostrarPokemons");
 // Se toma la constante de la data y la recorremos con el metodo .foreach que tiene la funcion de callback y el parametro es la data (archivo pokemon.js)
-function mostrarData (data){ 
+function mostrarData (dataAll){ 
   // se crea un cuerpo html a la seccion declarada como catalogo y se traen los datos ya declarados con template strings
-
   catalogo.innerHTML += `
     <div class="TarjetasPokemon">
       <div class="tarjetas">
       <div class="cuerpoTarjetas">
-      <img src="${data.img}"/>
-      <h3 class="idPokemon">${data.num}</h3><h3 class="nombrePokemon">${data.name.toUpperCase()}</h3>
-      <h4 class="peso">Peso: ${data.size.weight}</h4>
-      <h4 class="altura">Altura: ${data.size.height}</h4>
-      <h4 class="tipo">Tipo: ${data.type}</h4>
-      <p class="about"> ${data.about}</p>
+      <img src="${dataAll.img}"/>
+      <h3 class="idPokemon">${dataAll.num}</h3><h3 class="nombrePokemon">${dataAll.name.toUpperCase()}</h3>
+      <h4 class="peso">Peso: ${dataAll.size.weight}</h4>
+      <h4 class="altura">Altura: ${dataAll.size.height}</h4>
+      <h4 class="tipo">Tipo: ${dataAll.type}</h4>
+      <p class="about"> ${dataAll.about}</p>
       <p class=""></p>
     </div>
     </div>
@@ -74,61 +72,47 @@ dataPokemons.forEach(mostrarData);
 
 // funcion ordenar
 const selectorOrganizarAlfabeticamente = document.getElementById("ordenarAlfabeto");
-
 selectorOrganizarAlfabeticamente.addEventListener("change",function(){
   const opcionSeleccionada = this.options[selectorOrganizarAlfabeticamente.selectedIndex];
-
-  console.log(ordenarAlfabeto(dataPokemons, opcionSeleccionada.value))
-
+  const nuevaData = ordenarAlfabeto(dataPokemons, opcionSeleccionada.value);
+  catalogo.innerHTML="";
+  
+  if(opcionSeleccionada.value === "selecciona")
+  // mostrarData(dataPokemons);
+    dataPokemons.forEach(mostrarData);
+  else
+  // mostrarData(nuevaData);
+    nuevaData.forEach(mostrarData);
 });
-
-
 
 // funcion filtrar
 const SelectorElementoTipo = document.getElementById("filtroTipo");
-
 SelectorElementoTipo.addEventListener("change", function(){
   const opcionSeleccionada = this.options[SelectorElementoTipo.selectedIndex];
+  const catalogo2 =document.getElementById("pokemonesFiltrados");
+  const nuevaData = filtroData(dataPokemons, opcionSeleccionada.value);
+  catalogo2.innerHTML="";
 
-  console.log(filtroData(dataPokemons, opcionSeleccionada.value))
-  
+  function mostrarData2 (dataAll){     
+    catalogo2.innerHTML += `
+      <div class="TarjetasPokemon">
+        <div class="tarjetas">
+        <div class="cuerpoTarjetas">
+        <img src="${dataAll.img}"/>
+        <h3 class="idPokemon">${dataAll.num}</h3><h3 class="nombrePokemon">${dataAll.name.toUpperCase()}</h3>
+        <h4 class="peso">Peso: ${dataAll.size.weight}</h4>
+        <h4 class="altura">Altura: ${dataAll.size.height}</h4>
+        <h4 class="tipo">Tipo: ${dataAll.type}</h4>
+        <p class="about"> ${dataAll.about}</p>
+        <p class=""></p>
+      </div>
+      </div>
+      </div>
+      `;
+  }
+
+  if(opcionSeleccionada.value === "selecciona")
+    dataPokemons.forEach(mostrarData2);
+  else
+    nuevaData.forEach(mostrarData2);
 });
-
-
-
-
-
-
-//const catalogo3 =document.getElementById("page3");
-
-//const mostrarPokemon2 = function (data){ 
-// se crea un cuerpo html a la seccion declarada como catalogo y se traen los datos ya declarados con template strings
-   
-// catalogo3.innerHTML += `
-//   <div class="TarjetasPokemon">
-//   <div class="tarjetas">
-// <div class="cuerpoTarjetas">
-//<img src="${data.img}"/>
-//<h3 class="idPokemon">${data.num}</h3><h3 class="nombrePokemon">${data.name.toUpperCase()}</h3>
-// <h4 class="peso">Peso: ${data.size.weight}</h4>
-//<h4 class="altura">Altura: ${data.size.height}</h4>
-//<h4 class="tipo">Tipo: ${data.type}</h4>
-//<p class="about"> ${data.about}</p>
-//<p class=""></p>
-//</div>
-//</div>
-//</div>
-//`;
-//};
-
-//elementoTipo.addEventListener("change", function(){
-// if(elementoTipo.value === "selecciona"){
-//   mostrarPokemon2(dataPokemons);
-// }
-// else {
-//   mostrarPokemon2(filtroData(dataPokemons,elementoTipo.value));
-// }
-//});
-
-
-
