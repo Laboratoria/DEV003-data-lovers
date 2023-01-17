654aaecf8505f2ff966a4866f66a092422f339d4
RossannaAriza <rossanna.ariza@gmail.com>
2023-01-17 12:43:35 -0500

    agrega funciones, test,y grafica

--- src/data.js
+++ src/data.js
@@ -2,59 +2,65 @@
 export function ordenar(pokemons, orden){
   const organizadorSeleccionado = orden
   const ordenarDataAZ = pokemons.slice().sort((a,b) =>{
-    if (a.name.toLowerCase() < b.name.toLowerCase()){ 
+    if (a.name.toLowerCase() < b.name.toLowerCase()){
       return -1;
     }
-    if (a.name.toLowerCase() > b.name.toLowerCase()){ 
-      return 1;
-    }
-    return 0;
+    // if (a.name.toLowerCase() > b.name.toLowerCase()){
+    //   return 1;
+    // }
+    //return 0;
   });
   const ordenarDataZA = pokemons.slice().sort((a,b) =>{
-    if (a.name.toLowerCase() > b.name.toLowerCase()){ 
+    if (a.name.toLowerCase() > b.name.toLowerCase()){
       return -1;
     }
-    if (a.name.toLowerCase() < b.name.toLowerCase()){ 
-      return 1;
-    }
-    return 0;
+    // if (a.name.toLowerCase() < b.name.toLowerCase()){
+    //   return 1;
+    // }
+    //return 0;
   });
   const ordenarDataMenorMAyor = pokemons.slice().sort((a,b) =>{
-    if (a.num < b.num){ 
-      return -1;
-    }
-    if (a.num > b.num){ 
-      return 1;
-    }
-    return 0;
+    /*if*/ (a.num < b.num)/*{
+       return -1;
+     }
+     if (a.num > b.num){
+       return 1;
+     }
+    return 0;*/
   });
   const ordenarDataMayorMenor = pokemons.slice().sort((a,b) =>{
-    if (a.num > b.num){ 
+    if (a.num > b.num){
       return -1;
     }
-    if (a.num < b.num){ 
-      return 1;
-    }
-    return 0;
+    // if (a.num < b.num){
+    //   return 1;
+    // }
+    // return 0;
   });
-
+ 
   if (organizadorSeleccionado === "aZ"){
     return ordenarDataAZ;
   }
-  else if (organizadorSeleccionado === "zA"){
+  if (organizadorSeleccionado === "zA"){
     return ordenarDataZA;
-  } else if (organizadorSeleccionado === "ascendente"){
+  }
+  if (organizadorSeleccionado === "ascendente"){
     return ordenarDataMenorMAyor;
-  }else if (organizadorSeleccionado === "descendente"){
+  }
+  if (organizadorSeleccionado === "descendente"){
     return ordenarDataMayorMenor;
-  }  else {
-    return "";
   }
 }
-
+ 
 // funcion de filtrar
 export function filtroData (pokemons, condicion) {
   const elementoTipo = condicion
   const nuevaData = pokemons.filter((element) => element.type.includes(elementoTipo));
   return nuevaData;
+}
+ 
+//funcion estadistica
+export function estadistica (pokemons, poblacion){
+  const resumen = pokemons.filter((element) => element.generation.name.includes(poblacion));
+  return resumen;
 }
