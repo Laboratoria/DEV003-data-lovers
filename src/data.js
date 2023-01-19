7cb7a4f5429a9494202db7433a0c9181940cc6a6
Pamepvm <pamviveros@gmail.com>
2023-01-19 14:20:12 -0300

    segunda actualización

--- src/data.js
+++ src/data.js
@@ -1,9 +1,32 @@
-// estas funciones son de ejemplo
+/*import pokemon from './data/pokemon/pokemon.js';
+import data from './data/pokemon/pokemon.js'
 
-export const example = () => {
-  return 'example';
-};
+const arrayPokemones = data.pokemon*/
+
+export function filterByType(data, condition) {
+  const pokemonesByType = []
+  for (let i = 0; i <= data.length; i++) {
+    if (data[i] && data[i].type.includes(condition)) {
+      pokemonesByType.push(data[i])
+    }
+  }
+
+  return pokemonesByType
+}
+
+/*export function filterByType(data, typeSelection) {
+  for (let i = 0; i < arrayPokemones.length; i++) {
+    const pokemonesByType = [''];
+    if (data.type === typeSelection) {
+      pokemonesByType += pokemonesByType;
+
+    }
+    console.log(pokemonesByType);
+  }
+}
+
 
 export const anotherExample = () => {
-  return 'OMG';
-};
+  return 'OMG'
+      const element = array[i];
+}*/
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { filterByType } from './data.js'
+import pokemon from './data/pokemon/pokemon.js'
+import data from './data/pokemon/pokemon.js'
 
-console.log(example, data);
+function showListPokemons(pokemonlist) {
+  const contenedor = document.getElementById('contenedor')
+  contenedor.innerHTML = ''
+  pokemonlist.forEach((pokemon) => {
+    contenedor.innerHTML += `<div class="monos"> ${pokemon.type}</div>`
+  })
+
+  //console.log(filterByType);
+
+  const selectype = document.getElementById('filterByType')
+  //console.log("linea8",clickValidation);
+  selectype.addEventListener('change', () => {
+    const valueFilterByType = document.getElementById('filterByType').value
+    //console.log(valueFilterByType);
+    console.log(filterByType(Pokemones, valueFilterByType))
+  })
+}
+showListPokemons(data.pokemon)
