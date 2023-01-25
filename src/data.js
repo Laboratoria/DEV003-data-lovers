87b3760be5cb65e6668a190b4f6bdf51e4be1d26
melilotbulge <kleslie1982@gmail.com>
2023-01-25 13:37:17 -0600

    pruebaordenar

--- src/data.js
+++ src/data.js
@@ -1,9 +1,42 @@
-// estas funciones son de ejemplo
+import pokemon from './data/pokemon/pokemon.js';
+import data from './data/pokemon/pokemon.js'
 
-export const filterByType = (data,typeChose) => {
-  return data.filter(( pokemon ) => pokemon.type.includes(typeChose))
-};
+export function filterByType(data,condition) {
+  const pokemonesByType=[]
+  for (let i = 0; i <= data.length; i++) {
+  
+    if (data[i] && data[i].type.includes(condition)) {
+      pokemonesByType.push(data[i])
+    } 
+  } 
+  return pokemonesByType;
+}
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+let listadopokemon = data.pokemon
+
+console.log(listadopokemon);
+console.log();
+
+listadopokemon.sort((p1, p2) => {
+  if (p1.name < p2.name){
+      return -1;
+  }else if (p1.name>p2.marca ){
+      return 1;
+  }else{
+    return 0;
+  }
+});
+  
+ console.log(listadopokemon);
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,42 @@
-import { filterByType } from './data.js';
-// import data from './data/lol/lol.js';
+import { filterByType } from'./data.js';
+import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(filterByType, data);
+
+
+const dataPokemones=data.pokemon
+
+
+
+function showListPokemons(pokemonlist) {
+  const contenedor=document.getElementById("contenedor")
+  console.log(contenedor);
+  contenedor.innerHTML = ""
+  pokemonlist.forEach ( (pokemon) => {
+    contenedor.innerHTML += `<div class="tarjeta-pokemon"> 
+    <h4>${pokemon.type}</h4> 
+    <img class="imagenpokemon" src="${pokemon.img}">
+    <p class="nombre pokemon">${pokemon.name}</p>
+    </div>
+    `
+  } )
+  
+  }
+  showListPokemons(dataPokemones)
+
+  const selectUser=document.getElementById("filterByType");
+//console.log("linea8",clickValidation);
+selectUser.addEventListener("change", () => {
+    const valueFilterByType=selectUser.value;  
+  //console.log(valueFilterByType);
+  //console.log(filterByType(dataPokemones,valueFilterByType));
+  //showListPokemons(filterByType(dataPokemones,valueFilterByType));
+
+}
+)
+
+
+ 
+
+ 
+ 
